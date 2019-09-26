5fbe5afd3d86bf50f23ec4b954cbee3a1c050cff
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-09-26 17:44:50 +0200

    initial content

---
+++ func.asm
@@ -0,0 +1,50 @@
+;See top of main.cpp for "how to call these functions" from C++ code
+.CODE
+
+;This is just a function that return the number 42
+;Return values are "whatever is in RAX" in x64
+compute_answer PROC
+mov rax, 101010b
+ret
+compute_answer ENDP
+
+;The 4 first arguments are passed on by registers
+;The registers used for this purpose are
+; RCX - 1st arg
+; RDX - 2nd arg
+; R8  - 3rd arg
+; R9  - 4th arg
+sum_4_ints PROC
+mov rax, rcx
+add rax, rdx
+add rax, r8
+add rax, r9
+ret
+sum_4_ints ENDP
+
+;subsequent arguments are pushed on the stack.
+;however, there's also the return address and 4 "shadow" 64bit values
+;that correspond to the space the 4 first args would have occupied
+;If you do the maths, this means that 5th argument is 40 bytes up the sack,
+;and 6th is 48 bytes up the stack, etc...
+sum_6_ints PROC
+mov rax, rcx
+add rax, rdx
+add rax, r8
+add rax, r9
+add rax, [rsp + 40]
+add rax, [rsp + 48]
+ret
+sum_6_ints ENDP
+
+;pointers are naturally passed as arguments in the same way;
+;the following procedure act as the following C code would do
+; int increment_pointer_int(int* pointer) { *pointer++; }
+increment_pointer_int PROC
+add DWORD PTR [rcx], 1		;DWORD PTR [address] means that we are accessing a 32bit value
+ret
+increment_pointer_int ENDP
+
+
+
+END
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+//To simplify linking, we consider that the ASM defined functions are pure-C functions
+//And define the symbol that correspond to their name as an extern symbol with C linkage
+extern "C" int compute_answer();
+extern "C" int sum_4_ints(int, int, int, int); //this is a prototype, the arguments don't have names but you can put them there if you want :-)
+extern "C" int sum_6_ints(int, int, int, int, int, int);
+extern "C" void increment_pointer_int(int*);
+
+//The calling conventions are pretty much set in stone for x64, and correspond to "put 4 first args in registers, the rest on the stack"
+//See the assembly file comments for info about how to access these args
+
+
+int main()
+{
+    printf("%d\n", compute_answer());
+	printf("8 = %d\n", sum_4_ints(2,2,2,2));
+	printf("6 = %d\n", sum_6_ints(1,1,1,1,1,1));
+
+	int my_int = 0;
+	increment_pointer_int(&my_int);
+	printf("1 = %d\n", my_int);
+	return 0;
+}
