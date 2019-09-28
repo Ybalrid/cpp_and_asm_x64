6daf1adb8aa5a27a545d760da134b8585c86fe89
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-09-28 17:22:48 +0200

    clang-format

--- func.asm.hpp
+++ func.asm.hpp
@@ -9,15 +9,13 @@ extern "C" {
 
 // The calling conventions are pretty much set in stone for x64, and correspond
 // to "put 4 first args in registers, the rest on the stack" See the assembly
-// file comments for info about how to access these args  
+// file comments for info about how to access these args
 int compute_answer();
 int sum_4_ints(int, int, int, int); // this is a prototype, the arguments don't have names but you
                                     // can put them there if you want :-)
 int  sum_6_ints(int, int, int, int, int, int);
 void increment_pointer_int(int*);
-
 void swap(int*, int*);
-
 
 #ifdef __cplusplus
 } // end of extern "C" block
