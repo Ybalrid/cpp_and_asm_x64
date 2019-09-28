02964fd9c74ce76e1919eb7a2a6f5e1409451777
Arthur Brainville <ybalrid@ybalrid.info>
2019-09-28 17:18:43 +0200

    Adjust comments

--- func.asm.hpp
+++ func.asm.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
+// To permit linking, we consider that the ASM defined functions are pure-C
+// functions And define the symbol that correspond to their name as an extern
+// symbol with C linkage
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// To simplify linking, we consider that the ASM defined functions are pure-C
-// functions And define the symbol that correspond to their name as an extern
-// symbol with C linkage
+// The calling conventions are pretty much set in stone for x64, and correspond
+// to "put 4 first args in registers, the rest on the stack" See the assembly
+// file comments for info about how to access these args  
 int compute_answer();
 int sum_4_ints(int, int, int, int); // this is a prototype, the arguments don't have names but you
                                     // can put them there if you want :-)
@@ -15,9 +18,6 @@ void increment_pointer_int(int*);
 
 void swap(int*, int*);
 
-// The calling conventions are pretty much set in stone for x64, and correspond
-// to "put 4 first args in registers, the rest on the stack" See the assembly
-// file comments for info about how to access these args
 
 #ifdef __cplusplus
 } // end of extern "C" block
